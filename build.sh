53485d4642273f3aa1810d6ac9a6a4e76795e43d
QI-D <jonsondang@outlook.com>
2022-10-27 13:04:50 -0700

    update docker automation file
    Signed-off-by: QI-D <jonsondang@outlook.com>

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ docker build -t storage:latest storage/
 docker build -t processing:latest processing/
 docker build -t audit_log:latest audit_log/
 
-docker-compose up -f .deployment/docker-compose.yml -d
+cd deployment
+docker-compose up -d

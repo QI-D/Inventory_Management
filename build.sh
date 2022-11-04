8863e02397187227db795036464b574258bebddf
QI-D <jonsondang@outlook.com>
2022-11-04 10:52:24 -0700

    Containerize dashboard ui
    Signed-off-by: QI-D <jonsondang@outlook.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ docker build -t receiver:latest receiver/
 docker build -t storage:latest storage/
 docker build -t processing:latest processing/
 docker build -t audit_log:latest audit_log/
+docker build -t dashboard:latest dashboard-ui/
 
 cd deployment
 docker-compose up -d

6c7d8c17b203e3c8068eac14ef958a1de7218f8c
QI-D <jonsondang@outlook.com>
2022-11-19 22:39:47 -0800

    Config cors for health check service
    Signed-off-by: QI-D <jonsondang@outlook.com>

--- health_check/app.py
+++ health_check/app.py
@@ -9,6 +9,7 @@ import requests
 import time
 
 from apscheduler.schedulers.background import BackgroundScheduler
+from flask_cors import CORS, cross_origin
 
 
 def health():
@@ -49,6 +50,9 @@ def init_scheduler():
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
+
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 

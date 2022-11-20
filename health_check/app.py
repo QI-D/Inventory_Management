5811da5a8bfe39bb9143dbcbe7c79e7293486494
QI-D <jonsondang@outlook.com>
2022-11-19 22:25:48 -0800

    Create health check service
    Send get request to receiver, storage, processing, and audit_log
    services to check if the service is running. Saving the status to a json
    file.
    
    Signed-off-by: QI-D <jonsondang@outlook.com>

---
+++ health_check/app.py
@@ -0,0 +1,66 @@
+import connexion
+import datetime
+import json
+import swagger_ui_bundle
+import yaml
+import logging
+import logging.config
+import requests
+import time
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def health():
+    curr_time = datetime.datetime.now()
+    curr_time_str = curr_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+    health = {}
+
+    receiver_health = app_config["receiverHealth"]
+    storage_health = app_config["storageHealth"]
+    processing_health = app_config["processingHealth"]
+    audit_health = app_config["auditHealth"]
+    timeout = app_config["timeout"]
+
+    health_list = [receiver_health, storage_health, processing_health, audit_health]
+
+    for health_check in health_list:
+        try:
+            requests.get(health_check["url"], timeout=timeout)
+            health[health_check["service"]] = "Running"
+        except Exception as e:
+            health[health_check["service"]] = "Down"
+
+    health["last_updated"] = curr_time_str
+
+    logger.info(f"Health Check: {health}")
+
+    with open("health_check.json", "w+") as f:
+        json.dump(health, f, indent=4)
+
+    return health, 200
+
+def init_scheduler():
+    sched = BackgroundScheduler(daemon=True)
+    sched.add_job(health,
+                  'interval',
+                  seconds=app_config['scheduler']['period_sec'])
+    sched.start()
+
+
+app = connexion.FlaskApp(__name__, specification_dir='')
+
+app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
+
+with open("app_config.yml", 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+with open("log_config.yml", 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
+
+if __name__ == "__main__":
+    init_scheduler()
+    app.run(port=8120, use_reloader=False)

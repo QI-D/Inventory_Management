5eb773ac7d0167dd14337ab8d136369c23d671b1
QI-D <jonsondang@outlook.com>
2022-11-19 22:40:13 -0800

    Containerize health check service
    Signed-off-by: QI-D <jonsondang@outlook.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@ docker build -t storage:latest storage/
 docker build -t processing:latest processing/
 docker build -t audit_log:latest audit_log/
 docker build -t dashboard:latest dashboard-ui/
+docker build -t health_check:latest health_check/
 
-cd deployment
-docker-compose up -d
